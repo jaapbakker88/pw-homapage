edd4ab94e6e3ff5e539b85743ee1367f6c6b5d59
Jaap Bakker <hello@bakkerlabs.nl>
2017-10-17 16:55:08 +0200

    added ajax contact form sending code

--- static/js/custom.js
+++ static/js/custom.js
@@ -387,3 +387,27 @@ setInterval('cycleImages()', 4500);
 //     $('.tml-6').hide();
 //   }
 // });
+
+
+
+var $contactForm = $('.contact-form');
+$contactForm.submit(function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: '//formspree.io/info@partywith.co',
+    method: 'POST',
+    data: $(this).serialize(),
+    dataType: 'json',
+    beforeSend: function() {
+      $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+    },
+    success: function(data) {
+      $contactForm.find('.alert--loading').hide();
+      $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+    },
+    error: function(err) {
+      $contactForm.find('.alert--loading').hide();
+      $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+    }
+  });
+});
